305338ead075057f951f82bb94f32dae7b4cfc37
蔡俊喆 <13538265740@163.com>
2026-08-13 02:11:57 +0800

    feat(project): 初始化智能聊天项目基础配置

---
+++ src/main/java/com/junzhecai/SmartChatApplication.java
@@ -0,0 +1,11 @@
+package com.junzhecai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartChatApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SmartChatApplication.class, args);
+    }
+}
